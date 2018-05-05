1eb8f23bbebfdd1aa384f1633af11cffa1d7ad1b
Richard OReilly <roreilly@charitynetwork.com>
2018-05-05 02:48:54 -0400

    cleaner assets

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,46 @@
+$(function(){
+  function jumpTo($el) {
+    var speed = 1000
+    $('html, body').animate({
+         scrollTop: $el.offset().top
+     }, speed);
+
+  }
+
+
+  $(".top-nav ul li a").click(function() {
+    event.preventDefault();
+    // alert( "Handler for .click() called." );
+    alert( $(this).data("jump-to")  )
+    var jump_location = $(this).data("jump-to")
+    if ( jump_location == "top" ) {
+      jumpTo( $("body") )
+    } else if (jump_location == "current_work"){
+      jumpTo( $("#current-work") )
+    } else {
+      jumpTo( $("#sillyprojects") )
+    }
+
+
+
+
+  });
+
+  var waypoint = new Waypoint({
+    element: $(".nav-trigger"),
+    handler: function(direction) {
+      if(direction == "down"){
+        console.log('Scrolled to waypoint!', direction)
+        $(".top-nav").fadeIn()
+      } else {
+        console.log('Scrolled to waypoint!', direction)
+        $(".top-nav").fadeOut()
+      }
+
+
+    }
+  })
+
+
+
+});
